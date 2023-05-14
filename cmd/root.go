3b38f45d3bbe37882ca92a6ff2eb5719d55a2372
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-05-14 22:46:04 +0300

    create commands

--- cmd/root.go
+++ cmd/root.go
@@ -1,22 +1,18 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "portfolio-cli",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Use:   "portfolio",
+	Short: "Keeps track of your stock and crypto assets",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("My Portfolio")
+	},
 }
 
 func Execute() {
