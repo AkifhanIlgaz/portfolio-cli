d47d227cc981eb37e8ac42f0b3532f8857de62ab
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-05-16 13:05:03 +0300

    create db action functions

--- db/asset_actions.go
+++ db/asset_actions.go
@@ -8,3 +8,42 @@ var assetBucket = []byte("assets")
 	Stock
 	Delete
 */
+
+type AssetType int
+
+const (
+	Crypto = iota
+	Stock
+	Fiat
+)
+
+// Name will be key in DB
+type Asset struct {
+	Balance  float64
+	AvgPrice float64
+	Type     string
+}
+
+func AllAssets() []Asset {
+	panic("Return all assets that user has")
+}
+
+func Crpyto() []Asset {
+	panic("Return all cryptocurrencies that user has")
+}
+
+func Stocks() []Asset {
+	panic("Return all stocks that user has")
+}
+
+func DeleteAsset(name string) error {
+	panic("Delete given asset. Return error if asset doesn't exist")
+}
+
+func UpdateAsset(name string) error {
+	panic(`
+	Filter transactions by the name of given asset 
+	Calculate balance and average buy price
+	Delete if there is no transaction for this asset
+	`)
+}
--- db/transaction_actions.go
+++ db/transaction_actions.go
@@ -1,5 +1,7 @@
 package db
 
+import "time"
+
 var txBucket = []byte("transactions")
 
 /*
@@ -10,3 +12,45 @@ var txBucket = []byte("transactions")
 	All
 	AllTransactionForSameType ?
 */
+
+type txType int
+
+const (
+	buy = iota
+	sell
+)
+
+// Key will be sequential ID
+type Transaction struct {
+	Name   string
+	Date   time.Time `json:"date"`
+	Type   txType    `json:"type"`
+	Amount float64
+	Price  float64
+}
+
+func AllTransactions() []Transaction {
+	panic("Return all transactions")
+}
+
+func AllTransactionsOfAsset(asset string) []Transaction {
+	panic("Return all transactions of specified asset")
+}
+
+// Interactive ( Ask user what is the asset,price etc.)
+func CreateTransaction(tx Transaction) error {
+	panic("Create new transaction")
+}
+
+// Interactive
+func EditTransaction(id int) error {
+	panic("Edit existing transaction. Error if tx doesn't exist")
+}
+
+func DeleteTransaction(id int) error {
+	panic("Delete the transaction")
+}
+
+func GetTransaction(id int) (Transaction, error) {
+	panic("Return transaction by id")
+}
