5007f16cbfe52c03d49d94344920c720fc714fd8
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-05-21 17:03:45 +0300

    use tables for output

--- cmd/tx.go
+++ cmd/tx.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/AkifhanIlgaz/portfolio/db"
+	"github.com/rodaine/table"
 	"github.com/spf13/cobra"
 )
 
@@ -13,17 +14,21 @@ var txCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		// TODO: Show in table
 
+		tbl := table.New("ID", "Asset", "Type", "Amount", "Price", "Date")
+
 		if len(args) == 0 {
 			for _, tx := range db.AllTransactions() {
-				fmt.Println(tx)
+				tbl.AddRow(fmt.Sprintf("#%v", tx.ID), tx.Asset, tx.Type, tx.Amount, tx.Price, tx.Date.Format("2006-01-02 15:04"))
 			}
+			tbl.Print()
 			return
 		}
 
 		for _, asset := range args {
 			for _, tx := range db.AllTransactionsOfAsset(asset) {
-				fmt.Println(tx)
+				tbl.AddRow(fmt.Sprintf("#%v", tx.ID), tx.Asset, tx.Type, tx.Amount, tx.Price, tx.Date.Format("2006-01-02 15:04"))
 			}
+			tbl.Print()
 		}
 
 	},
